fc6aca2f437d8adbdc27efcaeea26c4e437ef51c
chenmj <1016219216@qq.com>
2022-07-29 11:24:48 +0800

    sb

--- src/page/dashboard/Dashboard.vue
+++ src/page/dashboard/Dashboard.vue
@@ -497,7 +497,7 @@ export default {
                 xAxis: {
                     type: 'category',
                     boundaryGap: false,
-                    data: Xdata,
+                    data: list.x,
                     splitLine: {
                         show: true,
                         lineStyle: {
